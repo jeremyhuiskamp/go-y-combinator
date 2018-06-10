26bfeaa08cd9b1fcb9b7395ef3934cc8ee03d3cf
Jeremy Huiskamp <jeremy.huiskamp@optiopay.com>
2018-06-10 20:43:27 +0200

    simplify by creating a `length` variable containing our complete function
    This is more idiomatic go, and easier to read.  While it now relies on
    statements rather than just expressions, it's technically trivial to
    revert this into scheme-compatible code, as long as we don't reference
    our variable more than once.

--- go/main.go
+++ go/main.go
@@ -9,25 +9,26 @@ type LengthFunc func([]string) int
 type LengthFuncFunc func(LengthFuncFunc) LengthFunc
 
 func main() {
-	fmt.Println(
-		func(makeLength func(LengthFunc) LengthFunc) LengthFunc {
-			return func(recurser LengthFuncFunc) LengthFunc {
-				return recurser(recurser)
-			}(
-				func(recurser LengthFuncFunc) LengthFunc {
-					return makeLength(func(list []string) int {
-						return recurser(recurser)(list)
-					})
-				},
-			)
+	length := func(makeLength func(LengthFunc) LengthFunc) LengthFunc {
+		return func(recurser LengthFuncFunc) LengthFunc {
+			return recurser(recurser)
 		}(
-			func(length LengthFunc) LengthFunc {
-				return func(list []string) int {
-					if len(list) == 0 {
-						return 0
-					}
-					return 1 + length(list[1:])
-				}
+			func(recurser LengthFuncFunc) LengthFunc {
+				return makeLength(func(list []string) int {
+					return recurser(recurser)(list)
+				})
 			},
-		)(os.Args[1:]))
+		)
+	}(
+		func(length LengthFunc) LengthFunc {
+			return func(list []string) int {
+				if len(list) == 0 {
+					return 0
+				}
+				return 1 + length(list[1:])
+			}
+		},
+	)
+
+	fmt.Println(length(os.Args[1:]))
 }
