df76dad5b5c2095da0f3fdaf5ba7f9ed0edff77a
Jeremy Huiskamp <jeremy.huiskamp@optiopay.com>
2018-06-10 19:55:39 +0200

    apply the y-combinator to our recursive function wrapper
    The result is an actual recursive function that will evaluate the length
    of an arbitrary list.

--- scheme/length.rkt
+++ scheme/length.rkt
@@ -1,12 +1,14 @@
 #lang racket
 
-(lambda (make-length)
-  ((lambda (recurser) (recurser recurser))
-   (lambda (recurser)
-     (make-length (lambda (list) ((recurser recurser) list))))))
+(
+ (lambda (make-length)
+   ((lambda (recurser) (recurser recurser))
+    (lambda (recurser)
+      (make-length (lambda (list) ((recurser recurser) list))))))
 
-(lambda (length)
-  (lambda (list)
-    (cond
-      ((null? list) 0)
-      (else (+ 1 (length (cdr list)))))))
+ (lambda (length)
+   (lambda (list)
+     (cond
+       ((null? list) 0)
+       (else (+ 1 (length (cdr list)))))))
+)
