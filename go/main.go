16d5845b932413f7df64acf37acbaed68f9e1abe
Jeremy Huiskamp <jeremy.huiskamp@optiopay.com>
2018-06-10 20:08:39 +0200

    introduce shell of the y-combinator and pass our recursive wrapper to it
    Now we see the type of the y-combinator for our specific use case. It:
    - accepts a function that
      - accepts a function that calculates length
      - returns a function that calculates length
    - returns a function that calculates length
    
    The input creates a function that handles one iteration through our
    input list.  The output is a fully recursive function that handles the
    entire list.

--- go/main.go
+++ go/main.go
@@ -3,12 +3,16 @@ package main
 type LengthFunc func([]string) int
 
 func main() {
-	_ = func(length LengthFunc) LengthFunc {
-		return func(list []string) int {
-			if len(list) == 0 {
-				return 0
+	_ = func(makeLength func(LengthFunc) LengthFunc) LengthFunc {
+		return nil
+	}(
+		func(length LengthFunc) LengthFunc {
+			return func(list []string) int {
+				if len(list) == 0 {
+					return 0
+				}
+				return 1 + length(list[1:])
 			}
-			return 1 + length(list[1:])
-		}
-	}
+		},
+	)
 }
