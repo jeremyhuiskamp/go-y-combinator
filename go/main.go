9bbbe98f12183d34bf60e7b0543b16be74ce42f5
Jeremy Huiskamp <jeremy.huiskamp@optiopay.com>
2018-06-10 20:03:39 +0200

    wrap recursive fragment in a function to allow binding the recursive target
    This compiles now, but isn't recursive yet.

--- go/main.go
+++ go/main.go
@@ -1,10 +1,12 @@
 package main
 
 func main() {
-	_ = func(list []string) int {
-		if len(list) == 0 {
-			return 0
+	_ = func(length func([]string) int) func([]string) int {
+		return func(list []string) int {
+			if len(list) == 0 {
+				return 0
+			}
+			return 1 + length(list[1:])
 		}
-		return 1 + length(list[1:])
 	}
 }
